54edffebb4d61a3a75ae9ca22d4a539af51520cf
Jay Bergonia <jay.b@tektosdesign.com>
2018-08-16 09:50:45 +0800

    Initial Commit

---
+++ BKG-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BKG-Test
+//
+//  Created by Jay Bergonia on 16/8/2018.
+//  Copyright © 2018 Tektos Limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
